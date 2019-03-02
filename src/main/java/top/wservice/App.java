ba408551691ad755e06379da82b3453236843649
shenhui12388 <18715857321@sina.cn>
2019-03-02 16:49:08 +0800

    创建

--- src/main/java/top/wservice/App.java
+++ src/main/java/top/wservice/App.java
@@ -18,6 +18,8 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 public class App extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(App.class,args);
     }
 
